35ac05b5f24d56a1918ab5741acf51e8e4fb65bc
Matti R <matti@mdranta.net>
2021-06-08 22:14:06 -0400

    update docs to hcloud

--- dist/index.js
+++ dist/index.js
@@ -5306,20 +5306,20 @@ const exec = __webpack_require__(986);
 const tc = __webpack_require__(533);
 const { Octokit } = __webpack_require__(889);
 
-const baseDownloadURL = "https://github.com/vultr/vultr-cli/releases/download"
-const fallbackVersion = "2.5.2"
+const baseDownloadURL = "https://github.com/hetznercloud/cli/releases/download"
+const fallbackVersion = "1.23.0"
 const octokit = new Octokit();
 
 async function downloadDoctl(version) {
     if (process.platform === 'win32') {
-        const doctlDownload = await tc.downloadTool(`${baseDownloadURL}/v${version}/vultr-cli_${version}_windows_64-bit.zip`);
+        const doctlDownload = await tc.downloadTool(`${baseDownloadURL}/v${version}/hcloud-windows-amd64.zip`);
         return tc.extractZip(doctlDownload);
     }
     if (process.platform === 'darwin') {
-        const doctlDownload = await tc.downloadTool(`${baseDownloadURL}/v${version}/vultr-cli_${version}_macOs_64-bit.tar.gz`);
-        return tc.extractTar(doctlDownload);
+        const doctlDownload = await tc.downloadTool(`${baseDownloadURL}/v${version}/hcloud-macos-amd64.zip`);
+        return tc.extractZip(doctlDownload);
     }
-    const doctlDownload = await tc.downloadTool(`${baseDownloadURL}/v${version}/vultr-cli_${version}_linux_64-bit.tar.gz`);
+    const doctlDownload = await tc.downloadTool(`${baseDownloadURL}/v${version}/hcloud-linux-amd64.tar.gz`);
     return tc.extractTar(doctlDownload);
 }
 
@@ -5328,8 +5328,8 @@ async function run() {
     var version = core.getInput('version');
     if ((!version) || (version.toLowerCase() === 'latest')) {
         version = await octokit.repos.getLatestRelease({
-            owner: 'vultr',
-            repo: 'vultr-cli'
+            owner: 'hetznercloud',
+            repo: 'cli'
         }).then(result => {
             return result.data.name;
         }).catch(error => {
@@ -5346,18 +5346,18 @@ Failed to retrieve latest version; falling back to: ${fallbackVersion}`);
         version = version.substr(1);
     }
 
-    var path = tc.find("vultr-cli", version);
+    var path = tc.find("hcloud", version);
     if (!path) {
         const installPath = await downloadDoctl(version);
-        path = await tc.cacheDir(installPath, 'vultr-cli', version);
+        path = await tc.cacheDir(installPath, 'hcloud', version);
     }
     core.addPath(path);
-    core.info(`>>> vultr-cli version v${version} installed to ${path}`);
+    core.info(`>>> hcloud version v${version} installed to ${path}`);
 
     var token = core.getInput('token', { required: true });
-    process.env.VULTR_API_KEY = token;
-    await exec.exec('vultr-cli account');
-    core.info('>>> Successfully installed vultr-cli and confirmed API key');
+    process.env.HCLOUD_TOKEN = token;
+    await exec.exec('hcloud datacenter list');
+    core.info('>>> Successfully installed hcloud and confirmed API key');
   }
   catch (error) {
     core.setFailed(error.message);
